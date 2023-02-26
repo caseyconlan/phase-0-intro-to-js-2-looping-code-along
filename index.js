f3225695fadbfe1ccf7b68df37126f95ceed22c4
Casey Conlan <caseyconlanshopping@gmail.com>
2023-02-26 09:54:20 -0800

    looping tests passed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', "Aki"]
+function writeCards(names, event) {
+    return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+ }
+ 
+ console.log(writeCards(names, "birthday"))
+
+function countDown(i) {
+    let integer = i; //typically use a different variable name than function name. i.e., don't use "countDown" for both function and variable
+    while (countDown >= 0) { //> is different from >=
+      console.log(countDown--);
+    }
+}
